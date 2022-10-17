24dd47ae05d5442cde337795d24c7e4c254f7a49
GrzegorzJendernal <g.jendernal@gmail.com>
2022-10-17 17:54:17 +0200

    added styled-components in Form module

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./style.css";
+import { StyledForm, Input, FormButton } from "./styled";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
@@ -17,15 +17,17 @@ const Form = ({ addNewTask }) => {
     };
 
     return (
-        <form className="form" onSubmit={onFormSubmit}>
-            <input
+        <StyledForm
+            onSubmit={onFormSubmit}
+        >
+            <Input
                 value={newTaskContent}
-                className="form__input"
                 placeholder="Co jest do zrobienia?"
                 onChange={({ target }) => setNewTaskContent(target.value)}
-                required />
-            <button className="form__button">Dodaj zadanie</button>
-        </form>
+                required
+            />
+            <FormButton>Dodaj zadanie</FormButton>
+        </StyledForm>
     );
 };
 
--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,37 +1,39 @@
-.form {
+import styled from "styled-components";
+
+const StyledForm = styled.form`
     padding: 20px;
     display: grid;
     grid-template-columns: 1fr auto;
     grid-gap: 20px;
-}
 
-.form__input {
+    @media (max-width: 767px) {
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-gap: 20px;
+    }
+`;
+
+const Input = styled.input`
     padding: 10px;
     cursor: none;
-}
+`;
 
-.form__button {
+const FormButton = styled.button`
     color: #ffffff;
     background-color: teal;
     padding: 10px;
     border: 0px;
     cursor: pointer;
-}
 
-.form__button:hover {
+    &:hover {
     background-color: hsl(180, 100%, 35%);
     transition: 1s;
     transform: scale(1.15);
-}
-
-.form__button:active {
-    outline: solid 2px #000000
-}
-
-@media (max-width: 767px) {
-    .form {
-        display: grid;
-        grid-template-columns: 1fr;
-        grid-gap: 20px;
     }
-}
+
+    &:active {
+    outline: solid 2px #000000
+    }
+`;
+
+export { StyledForm, Input, FormButton };
