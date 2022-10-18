774c94b3f96de2878dbef84c8ebb73befe1fbb9e
GrzegorzJendernal <g.jendernal@gmail.com>
2022-10-18 17:28:48 +0200

    added focus on input after add new task (useRef)

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { StyledForm, Input, FormButton } from "./styled";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+
+    const inputRef = useRef(null);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -21,12 +23,13 @@ const Form = ({ addNewTask }) => {
             onSubmit={onFormSubmit}
         >
             <Input
+                ref={inputRef}
                 value={newTaskContent}
                 placeholder="Co jest do zrobienia?"
                 onChange={({ target }) => setNewTaskContent(target.value)}
                 required
             />
-            <FormButton>Dodaj zadanie</FormButton>
+            <FormButton onClick={() => inputRef.current.focus}>Dodaj zadanie</FormButton>
         </StyledForm>
     );
 };
