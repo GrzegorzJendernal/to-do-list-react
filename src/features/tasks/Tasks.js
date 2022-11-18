ccd5a96891267945769d083448c6840318dd6f6b
GrzegorzJendernal <g.jendernal@gmail.com>
2022-11-18 19:51:18 +0100

    removed unnecessary constants

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,30 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import Form from "./Form";
 import List from "./List";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import { Main } from "../../common/Main/styled";
-import { selectTasks } from './List/tasksSlice';
 
-function Tasks() {
-  const { tasks } = useSelector(selectTasks);
 
-  return (
-    <Main>
-      <Header title="Lista zadań" />
-      <Section
-        title="Dodaj nowe zadanie"
-        body={<Form />}
-      />
-      <Section
-        title="Lista zadań"
-        optionalContent={<Buttons />}
-        body={<List />}
-      />
-    </Main>
-  );
-}
+const Tasks = () => (
+  <Main>
+    <Header title="Lista zadań" />
+    <Section
+      title="Dodaj nowe zadanie"
+      body={<Form />}
+    />
+    <Section
+      title="Lista zadań"
+      optionalContent={<Buttons />}
+      body={<List />}
+    />
+  </Main>
+);
 
 export default Tasks;
