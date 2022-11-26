eca971d241f16fd9922bb36847fdd0aea0f3b672
GrzegorzJendernal <g.jendernal@gmail.com>
2022-11-26 20:27:49 +0100

    visual changes

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -2,12 +2,13 @@ import styled from "styled-components";
 
 const StyledSection = styled.section`
     background-color: #ffffff;
+    margin:10px;
 `;
 
 const Subheader = styled.h2`
     padding: 20px;
     border-bottom: solid 2px #ededed;
-    margin: 0px;
+    margin: 5px;
     display: grid;
     grid-template-columns: 1fr auto auto;
 
