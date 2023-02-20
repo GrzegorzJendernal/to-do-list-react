2c8f727e993906e09826e1d90483907154ded16b
GrzegorzJendernal <g.jendernal@gmail.com>
2023-02-20 22:44:56 +0100

    typed tasksLocalStorage.ts

--- src/features/tasks/tasksLocalStorage.ts
+++ src/features/tasks/tasksLocalStorage.ts
@@ -1,7 +1,9 @@
+import {Task} from "../../common/types/interfaces";
+
 const localStorageKey = "tasks";
 
-export const saveTasksInLocalStorage = tasks =>
+export const saveTasksInLocalStorage = (tasks: Task[]) =>
 	localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 
 export const getTasksFromLocalStorage = () =>
-	JSON.parse(localStorage.getItem(localStorageKey)) || [];
+	JSON.parse(localStorage.getItem(localStorageKey) as string) || [];
