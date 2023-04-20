8cb7092b3e6dbdb4d52e7419a64c5d6756d8f591
GrzegorzJendernal <g.jendernal@gmail.com>
2023-04-20 21:26:53 +0200

    added test to incorrect task id

--- src/features/tasks/TaskPage/TaskPage.test.tsx
+++ src/features/tasks/TaskPage/TaskPage.test.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import TaskPage from "./index";
+import { MemoryRouter, Route } from "react-router-dom";
+import store from "../../../core/store";
+import { Provider } from "react-redux";
 
 describe("TaskPage", () => {
 	test("displays a message when task id is incorrect", () => {
-		render(<TaskPage/>);
-		expect(screen.getByText("Nie znaleziono zadania")).toBeInTheDocument();
-	})
+		render(
+			<Provider store={store}>
+			<MemoryRouter initialEntries={[`/tasks/non-existing-id`]}>
+				<Route path="/tasks/:id">
+					<TaskPage />
+				</Route>
+			</MemoryRouter>
+		</Provider>
+		);
+		expect(screen.getByText("Nie znaleziono zadania 😞")).toBeInTheDocument();
+	});
 });
