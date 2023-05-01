e2ffb43a452eeed36e8db247e3709e87a7adeaca
GrzegorzJendernal <g.jendernal@gmail.com>
2023-05-01 21:20:51 +0200

    created renderTaskPage function

--- src/features/tasks/TaskPage/TaskPage.test.tsx
+++ src/features/tasks/TaskPage/TaskPage.test.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import TaskPage from "./index";
-import { MemoryRouter, Route } from "react-router-dom";
-import store from "../../../core/store";
 import { Provider } from "react-redux";
+import { MemoryRouter, Route } from "react-router-dom";
+import TaskPage from "./index";
+import store from "../../../core/store";
 import { addTask } from "../tasksSlice";
 
 describe("TaskPage", () => {
+	const renderTaskPage = (taskId: string) => {
+		return {
+			...render (
+				<Provider store={store}>
+					<MemoryRouter initialEntries={[`/tasks/${taskId}`]}>
+						<Route path="/tasks/:id">
+							<TaskPage />
+						</Route>
+					</MemoryRouter>
+				</Provider>
+			)};
+	};
+
 	test("displays a message when task id is incorrect", () => {
 		const taskId = "non-existing-id";
-		render(
-			<Provider store={store}>
-			<MemoryRouter initialEntries={[`/tasks/${taskId}`]}>
-				<Route path="/tasks/:id">
-					<TaskPage />
-				</Route>
-			</MemoryRouter>
-		</Provider>
-		);
+
+		renderTaskPage(taskId);
 		expect(screen.getByText("Nie znaleziono zadania 😞")).toBeInTheDocument();
 	});
 
@@ -32,15 +38,7 @@ describe("TaskPage", () => {
 			done: false
 		}));
 
-		render(
-			<Provider store={store}>
-				<MemoryRouter initialEntries={[`/tasks/${taskId}`]}>
-					<Route path="/tasks/:id">
-						<TaskPage />
-					</Route>
-				</MemoryRouter>
-			</Provider>
-		);
+		renderTaskPage(taskId);
 		expect(screen.getByText("Szczegóły zadania")).toBeInTheDocument();
 		expect(screen.getByText(taskContent)).toBeInTheDocument();
 		expect(screen.getByText("Nie")).toBeInTheDocument();
