7f1580eb6f6df0dfb3abbea1c2c5e5baf2fda96d
GrzegorzJendernal <g.jendernal@gmail.com>
2023-05-04 20:45:24 +0200

    removed <nav> to other component

--- src/core/App/index.tsx
+++ src/core/App/index.tsx
@@ -3,23 +3,11 @@ import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import AuthorPage from "../../features/author/AuthorPage";
 import TaskPage from "../../features/tasks/TaskPage";
 import TasksPage from "../../features/tasks/TasksPage";
-import { NavList, StyledNavLink } from "./styled";
+import Navigation from "../../common/Navigation";
 
 const App = () => (
 	<HashRouter>
-		<nav>
-			<NavList>
-				<li>
-					<StyledNavLink to="/zadania">
-						Zadania
-					</StyledNavLink>
-				</li>
-				<li>
-					<StyledNavLink to="/autor">
-						O autorze
-					</StyledNavLink>
-				</li>
-			</NavList>
+		<Navigation/>
 			<Switch>
 				<Route path="/zadania/:id">
 					<TaskPage/>
@@ -34,7 +22,6 @@ const App = () => (
 					<Redirect to="/zadania"/>
 				</Route>
 			</Switch>
-		</nav>
 	</HashRouter>
 );
 
